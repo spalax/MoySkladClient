92092bd140dfcb9598977cca4391425f9200ac49
Milotskiy Alexey <spalaxinco@gmail.com>
2014-01-08 20:13:09 +0200

    Added new demo for changing customer order

---
+++ demo/ChangeCustomerOrders.php
@@ -0,0 +1,34 @@
+<?php
+namespace Zf2ClientMoysklad;
+
+require_once __DIR__.'/_config.php';
+
+use Zf2ClientMoysklad\Entity\CustomerOrder;
+use Zf2ClientMoysklad\Entity\Good;
+use Zf2ClientMoysklad\Entity\PurchaseOrder;
+use Zf2ClientMoysklad\Repository\BasicRepository;
+
+global $sm;
+
+/* @var $entityManager EntityManager */
+$entityManager = $sm->get('zf2clientmoysklad_entity_manager');
+
+/*====================== Method for sending payments ==========================*/
+
+/* @var $repository BasicRepository */
+$repository = $entityManager->getRepository('Zf2ClientMoysklad\Entity\CustomerOrder');
+
+/* @var $customerOrders CustomerOrder[] */
+$customerOrders = $repository->findAll(array('externalCode = '=>array('22684')));
+
+foreach ($customerOrders as $orderEntity) {
+    foreach ($orderEntity->getOrderPosition() as $orderPosition) {
+        /* @var CustomerOrder\Position $orderPosition */
+        $orderPosition->setQuantity(1199);
+    }
+    $entityManager->persist($orderEntity);
+}
+
+$entityManager->flush();
+
+/*---------------------------------------------------------------------------------*/
